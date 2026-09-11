9fc9d23c4c61f3551a3ff5ef2a761e98888ec2e9
Ryosuke-Kawashima-Career <ryosuke.kawashima.career@gmail.com>
2026-09-11 21:09:14 +0530

    Project selection is a part of max flow problem since it is all about making the two cuts inside the cluster. Directory flow_cut elaborates on this point.

--- flow_cut/node_edge_meaning.rs
+++ flow_cut/node_edge_meaning.rs
@@ -1,10 +1,10 @@
 use proconio::input;
 use proconio::marker::Chars;
-// 鉄則B69
+// 鉄則A69
 // エッジに意味を持たせる．
 // 0: start -> 1..=n: employees -> n+1..=n+24: working hours -> n+25: end
 fn main() {
-    input!{n: usize, m: i64, c: [Chars; n]}
+    input! {n: usize, m: i64, c: [Chars; n]}
     let end: usize = n + 25;
     let mut ff = FordFulkerson::new(end + 1);
     for employee in 1..=n {
@@ -14,7 +14,7 @@ fn main() {
     for employee in 1..=n {
         for j in 1..=24 {
             let time: usize = j + n;
-            if c[employee-1][j-1] == '1' {
+            if c[employee - 1][j - 1] == '1' {
                 ff.add_edge(employee, time, 1);
             }
         }
@@ -36,59 +36,63 @@ fn main() {
 struct Edge {
     to: usize,
     cap: i64,
-    rev: usize
+    rev: usize,
 }
 
 struct FordFulkerson {
     g: Vec<Vec<Edge>>,
-    used: Vec<bool>
+    used: Vec<bool>,
 }
 
 impl FordFulkerson {
     fn new(n: usize) -> FordFulkerson {
         let g: Vec<Vec<_>> = vec![vec![]; n];
         let used: Vec<bool> = vec![false; n];
-        FordFulkerson {g, used}
+        FordFulkerson { g, used }
     }
 
-    fn add_edge(&mut self, from:usize, to:usize, cap:i64) {
+    fn add_edge(&mut self, from: usize, to: usize, cap: i64) {
         let to_edge = Edge {
-            to, cap, rev: self.g[to].len()
+            to,
+            cap,
+            rev: self.g[to].len(),
         };
         self.g[from].push(to_edge);
         let from_edge = Edge {
-            to: from, cap: 0, rev: self.g[from].len() - 1
+            to: from,
+            cap: 0,
+            rev: self.g[from].len() - 1,
         };
         self.g[to].push(from_edge);
     }
 
-    fn dfs(&mut self, v:usize, t:usize, f:i64) -> i64 {
+    fn dfs(&mut self, v: usize, t: usize, f: i64) -> i64 {
         if v == t {
-            return f
+            return f;
         }
 
         self.used[v] = true;
         for i in 0..self.g[v].len() {
             let edge = self.g[v][i];
             if !self.used[edge.to] && 0 < edge.cap {
-            let d = self.dfs(edge.to, t, std::cmp::min(f, edge.cap));
+                let d = self.dfs(edge.to, t, std::cmp::min(f, edge.cap));
                 if 0 < d {
                     self.g[v][i].cap -= d;
                     self.g[edge.to][edge.rev].cap += d;
-                    return d
+                    return d;
                 }
             }
         }
         0
     }
 
-    fn max_flow(&mut self, s:usize, t:usize) -> i64 {
+    fn max_flow(&mut self, s: usize, t: usize) -> i64 {
         let mut flow = 0;
         loop {
             self.used = vec![false; self.used.len()];
             let f = self.dfs(s, t, std::i64::MAX);
             if f == 0 {
-                return flow
+                return flow;
             }
             flow += f;
         }
